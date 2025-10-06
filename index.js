8a7c65a8aa50ce48ca482b4348f2b0b91bdc79f3
Vision-Tey <nimusiimavision@gmail.com>
2025-10-06 12:15:16 +0300

    Add cors middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./db");
 
+const cors = require('cors')
 require('dotenv').config();
 const userRoutes = require("./routes/users");
 const postsRoutes = require("./routes/posts")
@@ -8,6 +9,15 @@ const postsRoutes = require("./routes/posts")
 const app = express();
 
 app.use(express.json());
+// Middleware to handle CORS
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res)=>{
     res.send('Welcome to my posts api')
